d3be2a7a1ccd0e8e0a5e2d98eaaf918f45a176bf
PhilLaptop <philngtn@gmail.com>
2021-08-02 15:58:56 -0400

    full coding

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      meaningOfLife: 7,
+    };
+  }
+  // 2nd way to declare state, we dont need the constructor for simple state
+  // state = {meaningOfLife:48}
+
+  // Props is added in the index.js to adding the props for the class App
+
+  handleClick = () => {
+    this.setState(
+      // 1st argument of setState
+      (prevState, prevProps) => {
+        return { meaningOfLife: prevState.meaningOfLife + prevProps.increment };
+      }, // 2nd Argument of setState "to see the result synchoronously"
+      () => console.log(this.state.meaningOfLife)
+    );
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.meaningOfLife}</p>
+          <button onClick={this.handleClick}>Update button</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
